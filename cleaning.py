ff7f3aa1d28fcaa0e9a4f9d2c2c6ee5e5ee6230e
Zach <zach.childers@gmail.com>
2020-09-24 20:58:55 -0500

    update to data cleaning code

--- cleaning.py
+++ cleaning.py
@@ -9,4 +9,28 @@ df_austin = df[df[“CityName”] == “Austin”]
 
 df_austin.reset_index().head()
 
-# df_austin.drop(columns=[])
+df_austin = df_austin.drop(
+    columns=[
+        “DataSource”,
+        “Data_Value_Footnote_Symbol”,
+        “Data_Value_Footnote”,
+        “CategoryID”,
+        “Short_Question_Text”,
+        “Data_Value_Unit”,
+        “DataValueTypeID”,
+        “Data_Value_Type”,
+        “Data_Value”,
+        “Low_Confidence_Limit”,
+        “High_Confidence_Limit”,
+        “CityFIPS”,
+    ]
+)
+df_austin = df_austin[df_austin[“GeographicLevel”] == “Census Tract”]
+df_austin[[“Lat”, “Lng”]] = df_austin.GeoLocation.apply(
+    lambda x: pd.Series(str(x).split(“,”))
+)
+df_austin[[“Lat”]] = df_austin.Lat.apply(lambda x: pd.Series(x[1:]))
+df_austin[[“Lng”]] = df_austin.Lng.apply(lambda x: pd.Series(x[:-1]))
+df_austin
+8:57
+df_austin.to_csv(“Data/cleaned_diabetes_cdc.csv”)
